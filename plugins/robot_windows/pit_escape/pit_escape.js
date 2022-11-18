9e4f34c3f0b6d151b9e7e169040f1f27aebfc0ca
Léo Duggan <leo.duggan@alumni.epfl.ch>
2022-11-18 14:08:58 +0100

    Updated files to new scenario

--- plugins/robot_windows/pit_escape/pit_escape.js
+++ plugins/robot_windows/pit_escape/pit_escape.js
@@ -1,7 +1,7 @@
 import RobotWindow from 'https://cyberbotics.com/wwi/R2022b/RobotWindow.js';
 
 window.robotWindow = new RobotWindow();
-const benchmarkName = 'Robot Programming';
+const benchmarkName = 'Pit Escape';
 let benchmarkPerformance = 0;
 
 const modal = document.querySelector(".modal");
@@ -21,24 +21,22 @@ closeButton.addEventListener("click", toggleModal);
 window.addEventListener("click", windowOnClick); 
 
 window.robotWindow.receive = function(message, robot) {
-  if (message.startsWith('percent:'))
-    document.getElementById('achievement').innerHTML = metricToString(parseFloat(message.substr(8)));
-  else if (message.startsWith('success:')) {
-    let benchmarkPerformance = message.split(':')[2]
-    const benchmarkPerformanceString = metricToString(benchmarkPerformance);
-    document.getElementById('achievement').innerHTML = benchmarkPerformanceString;
-    document.getElementById('achievement').style.color = 'green';
+  // updates the metric
+  if (message.startsWith('update:')) {
+    benchmarkPerformance = message.substr(7);
+    document.getElementById('performance-display').innerHTML = benchmarkPerformance;
+  } else if (message.startsWith('stop')) {
+    benchmarkPerformance = message.substr(5);
+    document.getElementById('performance-display').innerHTML = benchmarkPerformance;
+    document.getElementById('performance-display').style.color = 'green';
+    document.getElementById('performance-display').style.fontWeight = 'bold';
     document.querySelector(".text").innerHTML = `
       <h2>${benchmarkName} complete</h2>
       <h3>Congratulations you finished the benchmark!</h3>
-      <p>Your current performance is: <b style="color:green;">${benchmarkPerformanceString}</b></p>
+      <p>Your current performance is: <b style="color:green;">${benchmarkPerformance}%</b></p>
       <p>If you want to submit your controller to the leaderboard, follow the instructions given by the "Register" button on the benchmark page.</p>
     `
     toggleModal()
   } else
     console.log("Received unknown message for robot '" + robot + "': '" + message + "'");
-
-  function metricToString(metric) {
-    return (metric * 100).toFixed(2) + '%';
-  }
 };
