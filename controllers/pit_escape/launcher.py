10d5315caac718f7a269ada549e529c3e9a49c60
Léo Duggan <leo.duggan@alumni.epfl.ch>
2022-12-07 17:57:57 +0100

    Testing as subprocess

--- controllers/pit_escape/launcher.py
+++ controllers/pit_escape/launcher.py
@@ -4,4 +4,18 @@ import subprocess
 init = Robot()
 init.__del__()
 
-subprocess.call(["python", "pit_escape.py"])
+def _print_stdout(process):
+    realtime_output = process.stdout.readline()
+    if realtime_output:
+        print(realtime_output.strip())
+    return realtime_output
+
+controller = subprocess.Popen(
+    ["python", "pit_escape.py"],
+    stdout=subprocess.PIPE,
+    stderr=subprocess.STDOUT,
+    encoding='utf-8'
+)
+
+while controller.poll() is None:
+    _print_stdout(controller)
