f187ba352ee5dfd1b52b24ee7e32f726392faff3
Léo Duggan <leo.duggan@alumni.epfl.ch>
2022-12-09 09:32:26 +0100

    trying launcher solution

---
+++ controllers/pit_escape/bug.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+rm pit_escape.py
+cp pit_escape_bug.py pit_escape.py 
--- controllers/pit_escape/launcher.py
+++ controllers/pit_escape/launcher.py
@@ -1,21 +1,5 @@
 from controller import Robot
-import subprocess
 
-init = Robot()
-init.__del__()
+robot = Robot()
 
-def _print_stdout(process):
-    realtime_output = process.stdout.readline()
-    if realtime_output:
-        print(realtime_output.strip())
-    return realtime_output
-
-controller = subprocess.Popen(
-    ["python", "pit_escape.py"],
-    stdout=subprocess.PIPE,
-    stderr=subprocess.STDOUT,
-    encoding='utf-8'
-)
-
-while controller.poll() is None:
-    _print_stdout(controller)
+import pit_escape
